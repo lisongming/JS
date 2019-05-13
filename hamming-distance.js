0f529743d07a47c11492ce8c27f946b225f63ce6
lisongming <939200869@qq.com>
2019-05-14 00:00:14 +0800

    Create hamming-distance.js

---
+++ hamming-distance.js
@@ -0,0 +1,12 @@
+var hammingDistance = function(x, y) {
+    if(x==0&&y==0){
+        return 0;
+    }
+    var sum =x^y;
+    var i=0;
+    while(sum!=0){
+        sum=sum&(sum-1);
+        i++;
+    }
+    return i;
+}
