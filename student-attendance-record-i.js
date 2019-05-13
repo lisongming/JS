301a56103c0b70e951bb6c98a9a0a1f3f01d61fd
lisongming <939200869@qq.com>
2019-05-14 00:00:27 +0800

    Create student-attendance-record-i.js

---
+++ student-attendance-record-i.js
@@ -0,0 +1,12 @@
+var checkRecord = function(s) {
+    var s=0;
+    for(var i=0;i<s.length;i++){
+        if(s[i]=='A'){
+            return false;
+        }
+        if(s[i]=='LLL'){
+            return false;
+        }
+        return true
+    }
+};
